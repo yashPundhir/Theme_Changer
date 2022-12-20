3bba76d8dd4d3fe6a0078078289c0bdee23eed0e
yashPundhir <yash.pundhir.prof@gmail.com>
2022-12-20 17:27:37 +0530

    Navbar is updated wrt dark mode

---
+++ script.js
@@ -0,0 +1,44 @@
+function toggleLight() {
+	let body = document.querySelector("body");
+	let container = document.querySelector(".container");
+	body.style.backgroundColor = "#fff";
+	let lightBtn = document.querySelector(".theme .lightmode .lightmodebtn");
+	lightBtn.style.color = "#000";
+	let darkBtn = document.querySelector(".theme .darkmode .darkmodebtn");
+	darkBtn.style.color = "#000";
+	container.style.backgroundColor = "#fff";
+	container.style.color = "#101010";
+	let whiteLogo = document.querySelector(".whiteLogo");
+	let blackLogo = document.querySelector(".blackLogo");
+	blackLogo.style.display = "flex";
+	whiteLogo.style.display = "none";
+}
+function toggleDark() {
+	let body = document.querySelector("body");
+	let container = body.querySelector(".container");
+	body.style.backgroundColor = "#151515";
+	container.style.backgroundColor = "#151515";
+	container.style.color = "#fff";
+	let lightBtn = document.querySelector(".theme .lightmode .lightmodebtn");
+	let darkBtn = document.querySelector(".theme .darkmode .darkmodebtn");
+	let whiteLogo = document.querySelector(".whiteLogo");
+	let blackLogo = document.querySelector(".blackLogo");
+	let navLI = document.querySelectorAll(".nav-mid .list .list-item");
+	for (let i = 0; i < 6; i++) {
+		navLI[i].style.color = "#fff";
+		if (i === 0) {
+			navLI[i].style.fontWeight = "700";
+		} else {
+			navLI[i].style.fontWeight = "300";
+		}
+	}
+	let navbarBtn = document.querySelector(".nav-end .btn");
+	navbarBtn.style.border = "1.5px solid #fff";
+	navbarBtn.style.backgroundColor = "transparent";
+	navbarBtn.style.color = "#fff";
+
+	lightBtn.style.color = "#fff";
+	darkBtn.style.color = "#fff";
+	blackLogo.style.display = "none";
+	whiteLogo.style.display = "flex";
+}
